e0583ad15187e974972d821d929812c4bb300c39
Robert Sharp <39394881+robmsharp@users.noreply.github.com>
2023-02-17 15:02:28 +1100

    Update firebaseConfig.js

--- src/firebaseConfig.js
+++ src/firebaseConfig.js
@@ -6,13 +6,13 @@ import { getAuth} from "firebase/auth";
 
 
 const firebaseConfig = {
-  apiKey: "AIzaSyD6UCHmhvzzWQu16oUIwX_QxSFY3Ylpkdw",
-  authDomain: "slurm16-9621b.firebaseapp.com",
-  databaseURL: "https://slurm16-9621b-default-rtdb.firebaseio.com",
-  projectId: "slurm16-9621b",
-  storageBucket: "slurm16-9621b.appspot.com",
-  messagingSenderId: "1089297162853",
-  appId: "1:1089297162853:web:c6b725900c7a48af795c8c"
+  apiKey: "",
+  authDomain: "",
+  databaseURL: "",
+  projectId: "",
+  storageBucket: "",
+  messagingSenderId: "",
+  appId: ""
 };
 
 
